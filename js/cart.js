ce49131d6702efd37593e9b433777b230f213db7
Timothy Olili <timmolili27@gmail.com>
2020-12-14 16:58:54 +0300

    add interractivity to the buttons

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,5 @@
-//PART ONE - adding/loading products to the page
-
 // list of products to be listed for sale
-var products = [
+var productList = [
     {name:'Dior T-shirt', price:  28000, id: 0 },
     {name:'Armani Coat', price: 44000, id: 1 },
     {name:'High-top Nike23', price: 16000, id: 2 },
@@ -16,57 +14,74 @@ var products = [
     {name:'Gucci watch', price: 52300, id: 11 }
 ];
 
-var cart = []
+var cart = []; // array of selected items to be bought
 
+// This will popullate both products and cart lists in our page
+const popullateContainerWithItems = function(containerId, itemList){
+  var list = document.createElement('ul'); //create ul element to hold product list
 
-var listGroup = document.createElement('ul'); //create ul element
-
-document.getElementById('products').appendChild(listGroup); // get the element with id products and add the ul element to it
-
-var fireBtn;
-
-products.forEach(item => {
-let listItem = document.createElement('li'); // create li element
-listGroup.appendChild(listItem); // add li element to the ul element
-
-let spanEl = document.createElement('span'); //create span element
-spanEl.innerHTML = `${item.name}  <br /> UGX ${item.price}`; // add each product name and price to a span element
-listItem.innerHTML += spanEl.innerHTML + '<br />'; //add each span element to a li element
-
-fireBtn = document.createElement('button'); //create a button element
-fireBtn.className = 'fire-btn';
-fireBtn.id = item.id
-fireBtn.addEventListener('click', addToCart)
-
-let btn = document.createTextNode('Add to cart'); // create a textnode of add to cart
-fireBtn.appendChild(btn); // add textnode to button element
-listItem.appendChild(fireBtn); // add button to li element
-    });
-
-//end of adding products to the page
-
-
-
-//PART TWO - adding selected items to the cart section
-// fireBtn.addEventListener('click', addToCart)
-
-function addToCart(e) {
-
-// var cart = document.getElementById('cart');
-    // var selectedItems = [];
-    // cart.appendChild(selectedItems);
-
+  itemList.forEach(item => {
+    let itemContainer = document.createElement('li'); // creating the list item
+    let itemName = document.createElement('span'); // creating html element to house the product name
+    let actionButton = document.createElement('button'); //creating the button element
+    actionButton.id = item.id; //setting the id of each item to the id of a corresponding button
     
 
-    cart.push(products.find(product => product.id === Number.parseInt(e.target.id)))
-    console.log(cart)
-    alert('Item added succesfully')
+    // adding product details to our list items
+    itemName.innerText = item.name +  '\n UGX ' + item.price + '\n'; //adding the name and price of each item to a span element
 
-    
+    if (containerId === 'products'){
+      actionButton.innerText = 'Add to cart';
+      actionButton.addEventListener('click', addItemToCart);
+    } else {
+      actionButton.innerText = 'Remove from cart';
+      actionButton.addEventListener('click', removeFromCart);
+    }
+
+
+    itemContainer.appendChild(itemName); // adding name and price of each item to a li element
+    itemContainer.appendChild(actionButton); // adding a button to a li element
+    list.appendChild(itemContainer); // adding li element to a ul element
+
+    document.getElementById(containerId).appendChild(list); // find element with the id corresponding to the one passed in as an argument and add the ul element to it
+  });
+
+  
+  let orderButton = document.getElementById('order'); //get the button responsible for completing the order
+
+  if (cart.length > 0) {
+    orderButton.style.display = 'block';
+    } else {
+    orderButton.style.display = 'none';
+    }
+}
+
+const addItemToCart = function(e) {
+      
+    //find item to add to cart
+    cart.push(productList.find(product => product.id === Number.parseInt(e.target.id))); 
+    document.getElementById('cart').innerHTML = '';
+    popullateContainerWithItems('cart', cart);
 }
 
 
 
+const removeFromCart = function(e) {
+    // find item to remove from cart
+      let removeItem = cart.find(product => product.id === Number.parseInt(e.target.id));
+
+      // filter through cart and ignore item to remove
+      cart = cart.filter(item => item != removeItem);
+
+      // clear cart in our html
+      document.getElementById('cart').innerHTML = '';
+
+      // add our filtered items to the cart in the html
+      popullateContainerWithItems('cart', cart);
+
+}
 
 
+
+popullateContainerWithItems('products', productList);
 
